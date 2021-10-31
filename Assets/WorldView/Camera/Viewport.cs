9db5d54353495ec029df184a138c6e2705641304
Ty Cobb <ty.cobb.m@gmail.com>
2021-10-31 17:08:04 -0400

    use raw texture

--- Assets/WorldView/Camera/Viewport.cs
+++ Assets/WorldView/Camera/Viewport.cs
@@ -1,24 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// the camera's viewport
 [ExecuteAlways]
 public class Viewport: MonoBehaviour {
-    // -- tuning --
-    [Header("tuning")]
-    [Tooltip("the camera size in pixels")]
-    [SerializeField] float m_Size = 1.0f;
-
     // -- nodes --
     [Header("nodes")]
     [Tooltip("the camera")]
     [SerializeField] Camera m_Camera;
 
     // -- lifecycle --
-    void Update() {
-        var w = Screen.width;
-        var r = m_Camera.pixelRect;
-        r.x = w / 2.0f - m_Size;
-        r.width = m_Size;
-        m_Camera.pixelRect = r;
+    void Awake() {
+        // resize render texture to match screen height
+        // TODO: create new render texture on screen resize?
+        var tex  = m_Camera.targetTexture;
+        tex.height = Mathf.NextPowerOfTwo(Screen.height);
     }
 }
